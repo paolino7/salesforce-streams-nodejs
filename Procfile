30db1314820f060ff9f7523d3f623d14b102b24a
Paolo Zirotti <pzirotti@salesforce.com>
2020-12-22 14:13:21 +0100

    Base configuration for heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: node server.js
 stream_consumer: node stream-consumer.js
+pg_consumer: node pg-consumer.js
--- pg-consumer.js
+++ pg-consumer.js
@@ -1,2 +1,48 @@
+require("dotenv").config();
 const redis = require('redis');
 const { Pool } = require('pg');
+
+const {
+    DB_USERNAME,
+    DB_HOST,
+    DB_DATABASE,
+    DB_PASSWORD,
+    DB_PORT
+} = process.env;
+if (
+    !(
+        DB_USERNAME &&
+        DB_HOST &&
+        DB_DATABASE &&
+        DB_PASSWORD &&
+        DB_PORT
+    )
+) {
+    console.error(
+        "Cannot start app: missing mandatory configuration. Check your .env file."
+    );
+    process.exit(-1);
+}
+
+const pool = new Pool({
+    user: DB_USERNAME,
+    host: DB_HOST,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+(async () => {
+
+    const pg_client = await pool.connect();
+
+    await pg_client.query('SELECT NOW() as now').then(res => {
+        console.log(res.rows[0]);
+    }).catch(err => {
+        console.error(err.stack);
+    })
+
+})().catch((err) => console.error(err));
